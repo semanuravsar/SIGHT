ef22a6dabd3dd2df11ef8097b5387b1385727a2c
Erdem <erdemcanaz@gmail.com>
2023-11-22 22:26:27 +0300

    kinda works

--- communication unit/receiver/receiver.ino
+++ communication unit/receiver/receiver.ino
@@ -1,16 +1,35 @@
-#include <IRremote.h>
+#define IR_RECEIVE_PIN 3
+#define BAUD_RATE 115200
 
-IRrecv IR(3);
-
-void setup(){
-  IR.enableIRIn();
-  Serial.begin(9600);
+void setup() {
+  pinMode(IR_RECEIVE_PIN, INPUT);
+  Serial.begin(BAUD_RATE);
 }
 
-void loop(){
-  if(IR.decode()){
-    Serial.println(IR.decodedIRData.decodedRawData , HEX);
-    delay(1000);
-    IR.resume();
+void loop() { 
+  count_IR();
+}
+
+
+void count_IR(){
+  int transitionCount = 0;  // Counter for the number of high-to-low transitions
+
+  if(0 == digitalRead(IR_RECEIVE_PIN)){
+    uint8_t last_IR_state = 0;
+    unsigned long start_time = millis();
+    while(millis()-start_time<18){
+      uint8_t IR_read =  digitalRead(IR_RECEIVE_PIN);
+      if(last_IR_state != IR_read){
+          last_IR_state = IR_read;
+          transitionCount = transitionCount +1;
+      }
+    }
+    transitionCount = (transitionCount +1)>>1;
+    Serial.println(transitionCount);
   }
+
 }
+
+
+
+
--- communication unit/transmitter/transmitter.ino
+++ communication unit/transmitter/transmitter.ino
@@ -1,20 +1,38 @@
-#include <IRremote.h>
+#define IR_LED 5
+void send_IR(uint8_t);
 
-// Define the IR LED pin
-const int irLedPin = 8; // Change this to the appropriate pin number
-
-// Define the custom IR code (you can replace this with your own code)
-unsigned long customCode = 0x20DF40BF;
-
-void setup() {
-  // Initialize the IR LED pin
-  pinMode(irLedPin, OUTPUT);
-
+void setup(){
+  pinMode(IR_LED, OUTPUT);
 }
 
-void loop() {
-  // Send the custom IR code
-  IrSender.sendNEC(customCode, 32); // 32 is the number of bits
-  // Wait for a moment before sending the next code
-  delay(2); // 5-second delay (adjust as needed)
+void loop(){
+  // for(int i = 1; i<10; i++){
+  //   send_IR(i);
+  // }
+  send_IR(5);
+  send_IR(3);
+  send_IR(7);
+  send_IR(1);
+  send_IR(1);
+  send_IR(7);
+  send_IR(3);
+  send_IR(5);
+  delay(2000);
 }
+
+
+void send_IR(uint8_t val_to_send){
+  //total required time = val_to_send * 1.2ms
+  for (int k=0;k<(val_to_send);k++){
+      unsigned long start_time = micros();
+      while(micros()-start_time <600){
+        digitalWrite(IR_LED, LOW);
+        delayMicroseconds(10);
+        digitalWrite(IR_LED, HIGH);
+        delayMicroseconds(10);
+      }
+      delayMicroseconds(600);
+  }
+  delay(20);
+}
+
