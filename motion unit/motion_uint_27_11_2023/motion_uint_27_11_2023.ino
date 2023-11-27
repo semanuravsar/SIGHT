5dd6c18ff26776d040ea0a8a48131aa112a352fb
Erdem <erdemcanaz@gmail.com>
2023-11-27 15:57:43 +0300

    Update motion_uint_27_11_2023.ino

--- motion unit/motion_uint_27_11_2023/motion_uint_27_11_2023.ino
+++ motion unit/motion_uint_27_11_2023/motion_uint_27_11_2023.ino
@@ -1,35 +1,61 @@
-    /*
-  L298N DC MOTOR DRIVER MODULE
-  modified on 25 Sep 2020
-  by Saeed Olfat @ Electropeak
-  Home
-*/
+
 
 void setup() {
   pinMode(8, OUTPUT); //IN2
   pinMode(9, OUTPUT); //IN1
-  pinMode(10, OUTPUT); //Enable Pin
-}
-void loop() {
+  pinMode(10, OUTPUT); //Enable Pin (IN1 & IN2)
+  pinMode(6,OUTPUT);  // IN3
+  pinMode(7, OUTPUT); //IN4
+  pinMode(11,OUTPUT); // Enable Pin (IN3 & IN4)
   delay(3000);
 
-// 0 to 100% speed in forward mode
-  for (int i=0;i<256;i++)
-  {   digitalWrite(8, HIGH);
-      digitalWrite(9, LOW);
-      analogWrite(10, i);
-      delay(50);      
-      }
+}
+void loop() {
 
-  delay(50);
+// 0 to 100% speed in forward mode
+  for (int i=0;i<128;i++)
+  {   
+      right_forward(i);
+      left_backward(i);
+      delay(50);      
+  }
 
   // 0 to 100% speed in backward mode
-        for (int i=0;i<256;i++)
-  {   digitalWrite(8, LOW);
-      digitalWrite(9, HIGH);
-      analogWrite(10, i);
-      delay(50);      
-      }
+  for (int i=0;i<128;i++)
+  {   
+      right_backward(i);
+      left_forward(i);
+      delay(50);
+  }
 
-        delay(50);
 }
+
+
+void right_forward(uint8_t pwm_val){
+  digitalWrite(8, HIGH);
+  digitalWrite(9, LOW);
+  analogWrite(10, pwm_val);
+}
+
+
+void right_backward(uint8_t pwm_val){
+  digitalWrite(8, LOW);
+  digitalWrite(9, HIGH);
+  analogWrite(10, pwm_val);
+}
+
+void left_forward(uint8_t pwm_val){
+  digitalWrite(6, HIGH);
+  digitalWrite(7, LOW);
+  analogWrite(11, pwm_val);
+}
+
+void left_backward(uint8_t pwm_val){
+  digitalWrite(6, LOW);
+  digitalWrite(7, HIGH);
+  analogWrite(11, pwm_val);
+}
+
+
+
+
