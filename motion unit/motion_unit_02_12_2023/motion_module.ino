8d989ca7f65e935cb177374cc27eafc802bd9bff
Erdem <erdemcanaz@gmail.com>
2023-12-02 17:35:10 +0300

    test functions related to motion unit is finished

---
+++ motion unit/motion_unit_02_12_2023/motion_module.ino
@@ -0,0 +1,127 @@
+#define in_1 8    //IN1
+#define in_2 9    //IN2
+#define en_12 10  //Enable Pin (IN1 & IN2)
+#define in_3 6    //IN3
+#define in_4 7    //IN4
+#define en_34 11  //Enable Pin (IN3 & IN4)
+#define builtin_LED 13
+#define battery_voltage_pin A0
+
+//Global variables
+const int motor_rated_voltage = 6;
+int MAX_PWM_ALLOWED = 255;  //by default
+float BATTERY_VOLTAGE = 5;  //by default
+
+//=================================================
+void initialize_motor_pins() {
+  pinMode(builtin_LED, OUTPUT);
+  for (uint8_t i = 0; i < 4; i++) {
+    digitalWrite(builtin_LED, HIGH);
+    delay(250);
+    digitalWrite(builtin_LED, LOW);
+    delay(250);
+  }
+  pinMode(in_1, OUTPUT);
+  pinMode(in_2, OUTPUT);
+  pinMode(en_12, OUTPUT);
+  pinMode(in_3, OUTPUT);
+  pinMode(in_4, OUTPUT);
+  pinMode(en_34, OUTPUT);
+  pinMode(battery_voltage_pin, INPUT);
+  delay(1000);
+}
+void test_measure_battery_voltage(float reduction_ratio) {
+  Serial.println("\nreading battery voltage with reduction ratio of: " + String(reduction_ratio));
+  int reading = analogRead(battery_voltage_pin);
+  Serial.println("reading is: " + String(reading) + " / 1023");
+  float actual_voltage = (5.0 * reading / 1024.0) / reduction_ratio;
+  Serial.println("Actual battery voltage is: " + String(actual_voltage) + " V");
+}
+
+int test_set_max_pwm_allowed(float battery_voltage) {
+  int max_pwm_allowed = 0;
+  if (battery_voltage > 0) {
+    Serial.println("\n Calculating max-pwm allowed for battery voltage of " + String(battery_voltage));
+    max_pwm_allowed = floor((motor_rated_voltage / battery_voltage) * 255);  //do not use round because it may round 255.X to 256. Which is not expected by the analog write
+    Serial.println("Max pwm is calculated as " + String(max_pwm_allowed));
+  } else {
+    Serial.println("\n Could not calculate max-pwm allowed for battery voltage of " + String(battery_voltage));
+  }
+  return max_pwm_allowed;
+}
+void test_motor() {
+  delay(5000);
+  int pwm_i = 0;
+  int step_size = 1;
+  int pwm_min = -256;
+  int pwm_max = 256;
+  int deltaT = 25;
+  int battery_voltage = 12;
+
+  float rated_pwm_max = test_set_max_pwm_allowed(battery_voltage);
+
+  int current_pwm = pwm_i;
+  Serial.println("\ntesting starts");
+  Serial.println("\ndecrease current pwm");
+  for (int i = current_pwm; i > pwm_min; i = i - step_size) {
+    test_drive_right_motor(i, rated_pwm_max);
+    test_drive_left_motor(i, rated_pwm_max);
+    current_pwm = i;
+    delay(deltaT);
+  }
+  Serial.println("\nincrease current pwm");
+  for (int i = current_pwm; i < pwm_max; i = i + step_size) {
+    test_drive_right_motor(i, rated_pwm_max);
+    test_drive_left_motor(i, rated_pwm_max);
+    current_pwm = i;
+    delay(deltaT);
+  }
+  Serial.println("\ndecrease current pwm");
+  for (int i = current_pwm; i > 0; i = i - step_size) {
+    test_drive_right_motor(i, rated_pwm_max);
+    test_drive_left_motor(i, rated_pwm_max);
+    current_pwm = i;
+    delay(deltaT);
+  }
+  Serial.println("\ntesting ends");
+}
+
+void test_drive_right_motor(int pwm_val, int max_pwm_allowed) {
+  Serial.println("\ncalling test_drive_right_motor() with pwm_val of:" + String(pwm_val));
+  if ((0 <= pwm_val) && (pwm_val <= max_pwm_allowed)) {
+    digitalWrite(in_1, HIGH);
+    digitalWrite(in_2, LOW);
+    analogWrite(en_12, pwm_val);
+    Serial.println("forward motion");
+  } else if ((-max_pwm_allowed <= pwm_val) && (pwm_val < 0)) {
+    digitalWrite(in_1, LOW);
+    digitalWrite(in_2, HIGH);
+    analogWrite(en_12, -pwm_val);
+    Serial.println("backward motion");
+  } else {
+    digitalWrite(in_1, LOW);
+    digitalWrite(in_2, LOW);
+    analogWrite(en_12, 0);
+    Serial.println("index exceeded expected range, stop the motor!");
+  }
+}
+
+void test_drive_left_motor(int pwm_val, int max_pwm_allowed) {
+  Serial.println("\ncalling test_drive_left_motor() with pwm_val of:" + String(pwm_val));
+  if ((0 <= pwm_val) && (pwm_val <= max_pwm_allowed)) {
+    digitalWrite(in_3, LOW);
+    digitalWrite(in_4, HIGH);
+    analogWrite(en_34, pwm_val);
+    Serial.println("forward motion");
+  } else if ((-max_pwm_allowed <= pwm_val) && (pwm_val <= 0)) {
+    digitalWrite(in_3, HIGH);
+    digitalWrite(in_4, LOW);
+    analogWrite(en_34, -pwm_val);
+    Serial.println("backward motion");
+  } else {
+    digitalWrite(in_3, LOW);
+    digitalWrite(in_4, LOW);
+    analogWrite(en_34, 0);
+    Serial.println("index exceeded expected range, stop the motor!");
+  }
+}
