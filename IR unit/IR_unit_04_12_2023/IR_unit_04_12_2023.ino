4c22efa935fe94eb59b8f8d4eba460eaa03c7ef7
Erdem <erdemcanaz@gmail.com>
2023-12-04 20:03:22 +0300

    Faster bit rate trials.

--- IR unit/IR_unit_04_12_2023/IR_unit_04_12_2023.ino
+++ IR unit/IR_unit_04_12_2023/IR_unit_04_12_2023.ino
@@ -4,18 +4,20 @@ void setup() {
 
   uint8_t data[]= {
     1,1,1,1,0,1,0,0,0,0,
+    1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,0,1,0,0,0,0,
     1,1,1,1,1,1,1,1,1,1
     };
 
-  for(uint8_t i = 0; i< 20 ; i++){
+  for(uint8_t i = 0; i< 40 ; i++){
     set_IR_module_buffer(i,data[i]);
   }
 
 }
 
 void loop() {
-  //receive_package_if_possible();
-  transmit_package();
-  delay(50);
+  receive_package_if_possible();
+  //transmit_package();
+  //delay(25);
 
 }
