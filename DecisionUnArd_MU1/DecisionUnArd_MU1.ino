170608d1495bba6670d366b5633a6506cf0c1626
Humeyra Bodur <78879948+humeyrabodur@users.noreply.github.com>
2024-05-18 22:08:03 +0300

    Decision Unit Codes for MU1

---
+++ DecisionUnArd_MU1/DecisionUnArd_MU1.ino
@@ -0,0 +1,323 @@
+//  The values info matrix represent the following;
+//  mu1 is here == 1
+//  mu2 is here == 2
+//  mu3 is here == 3
+//  BU is here == 4
+//  This tile is scanned == 5
+//  This tile has not been scanned == 0
+//  The target is there == 6
+//  mu is planning to go here == 7
+//  obtacle here == 8
+
+#include "DecisionUnArd_header.h";
+#include "IR_module_header.h";
+
+// RFID Includes
+#include <SPI.h>
+#include <MFRC522.h>
+//RFID defines
+#define RST_PIN 3
+#define SS_PIN 10
+#define TALK_PIN 2
+MFRC522 rfid(SS_PIN, RST_PIN);
+
+int LED_PIN = 5;
+
+
+// Define positions
+Position buPosition = {5, 5};
+Position currentPosition = {-1, -1};
+Position previousPosition = {-1, -1};
+// Position unknownTargetPosition = {1, 9};
+Position targetPosition = {20,20};
+Position MU1_NextPosition = {20, 20};
+Position MU2_NextPosition = {20, 20};
+Position MU3_NextPosition = {20, 20};
+uint8_t MU1_state = 0;
+uint8_t MU2_state = 0;
+uint8_t MU3_state = 0;
+uint8_t ID = 1;
+
+// COMM function variables
+uint8_t loop_iteration_wo_ping_message;
+uint8_t ping_time_out = 0;
+uint8_t ping_timer = millis();
+uint8_t active_s = 1;
+
+
+// Define obstacles
+Position obstacles[MAX_OBSTACLES];
+int obstacleCount = 2;
+int initial_obstacleCount = 2;
+
+int buLink = 0; 
+int ALERT_BU = 0;
+// Define mu_info_matrix
+int mu_info_matrix[GRID_SIZE][GRID_SIZE] = {
+    {0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {8, 0, 0, 0, 8, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {7, 7, 7, 7, 7, 7, 7, 7, 7},
+    {7, 7, 7, 7, 4, 7, 7, 7, 7},
+    {7, 7, 7, 7, 7, 7, 7, 7, 7},
+    {7, 7, 7, 7, 7, 7, 7, 7, 7},
+    {7, 7, 7, 7, 7, 7, 7, 7, 7},
+    {7, 7, 7, 7, 7, 7, 7, 7, 7}
+};
+
+// Set state to MOVING_TO_SEARCH_AREA
+int state = MOVING_TO_SEARCH_AREA;
+
+uint8_t move_decider_wo_comm() {
+
+  uint8_t mu_move = 0;
+
+  buLink = bu_link_checker(currentPosition, buPosition);
+
+  // Serial.println("State when entering the move decider: ");
+  // Serial.println(state);
+
+  if(state ==  TARGET_FOUND && buLink && (currentPosition.x == targetPosition.x && currentPosition.y == targetPosition.y) ){
+    state = TARGET_REACHED;
+    mu_move = 5;
+    // Serial.println("Girdi buraya");
+    return mu_move;
+    ALERT_BU = 1;
+  }
+
+  if(state == TARGET_FOUND && buLink){
+    state = MOVING_TO_TARGET;
+    ALERT_BU = 1;
+  }
+
+  // Updating the info matrix
+  if(currentPosition.x == targetPosition.x && currentPosition.y == targetPosition.y){mu_info_matrix[currentPosition.x-1][currentPosition.y-1] = 6;}
+  else{mu_info_matrix[currentPosition.x-1][currentPosition.y-1] = 5;}
+
+  // If our state is searching and we have no more unscanned tiles left
+  if(state == CURRENTLY_SEARCHING && !unscannedTileLeft(mu_info_matrix)){
+    state = TARGET_NOT_FOUND;
+  }
+
+  if(state == MOVING_TO_TARGET && (currentPosition.x == targetPosition.x) && (currentPosition.y == targetPosition.y)){
+    state = TARGET_REACHED;
+    mu_move = 5;
+    return mu_move;
+  }
+
+  if(state == MOVING_TO_SEARCH_AREA){
+    if((currentPosition.x < 4) && (currentPosition.y > 0)){
+      state = CURRENTLY_SEARCHING; 
+    }
+  }
+
+  // Call move_decider_MU1
+  Position nextMoveResult = move_decider_MU1(buPosition, currentPosition, targetPosition, obstacles, obstacleCount, mu_info_matrix, state, buLink);
+  MU1_NextPosition.x = currentPosition.x + nextMoveResult.x;
+  MU1_NextPosition.y = currentPosition.x + nextMoveResult.x;
+
+  // 0 == STOP
+  // 1 == FORWARDS
+  // 2 == TURN RIGHT
+  // 3 == TURN LEFT
+  // 4 == TURN AROUND
+  // 5 == TERMINATE SEARCH
+
+  // If we are just starting the search
+  if(previousPosition.x == -1){
+ 
+    if(nextMoveResult.x == 1 && nextMoveResult.y == 0){mu_move = 1;}
+    else if(nextMoveResult.x == -1 && nextMoveResult.y == 0){mu_move = 4;}
+    else if(nextMoveResult.x == 0 && nextMoveResult.y == 1){mu_move = 3;}
+    else if(nextMoveResult.x == 0 && nextMoveResult.y == -1){mu_move = 2;}
+    else{mu_move = 0;}
+
+  }
+  else{
+    Position orientation;
+    orientation.x = currentPosition.x - previousPosition.x;
+    orientation.y = currentPosition.y - previousPosition.y;
+
+    //ORIENTATION = RIGHT
+    if(orientation.x == 0 && orientation.y == 1){
+      if(nextMoveResult.x == 1 && nextMoveResult.y == 0){mu_move = 2;}
+      else if(nextMoveResult.x == -1 && nextMoveResult.y == 0){mu_move = 3;}
+      else if(nextMoveResult.x == 0 && nextMoveResult.y == 1){mu_move = 1;}
+      else if(nextMoveResult.x == 0 && nextMoveResult.y == -1){mu_move = 4;}
+      else{mu_move = 0;}
+    }
+    //ORIENTATION = LEFT
+    else if(orientation.x == 0 && orientation.y == -1){
+      if(nextMoveResult.x == 1 && nextMoveResult.y == 0){mu_move = 3;}
+      else if(nextMoveResult.x == -1 && nextMoveResult.y == 0){mu_move = 2;}
+      else if(nextMoveResult.x == 0 && nextMoveResult.y == 1){mu_move = 4;}
+      else if(nextMoveResult.x == 0 && nextMoveResult.y == -1){mu_move = 1;}
+      else{mu_move = 0;}
+    }
+    //ORIENTATION = FORWARD (DEFAULT)
+    else if(orientation.x == 1 && orientation.y == 0){
+      if(nextMoveResult.x == 1 && nextMoveResult.y == 0){mu_move = 1;}
+      else if(nextMoveResult.x == -1 && nextMoveResult.y == 0){mu_move = 4;}
+      else if(nextMoveResult.x == 0 && nextMoveResult.y == 1){mu_move = 3;}
+      else if(nextMoveResult.x == 0 && nextMoveResult.y == -1){mu_move = 2;}
+      else{mu_move = 0;}
+    }
+
+    //ORIENTATION = BACKWARDS
+    else if(orientation.x == -1 && orientation.y == 0){
+      if(nextMoveResult.x == 1 && nextMoveResult.y == 0){mu_move = 4;}
+      else if(nextMoveResult.x == -1 && nextMoveResult.y == 0){mu_move = 1;}
+      else if(nextMoveResult.x == 0 && nextMoveResult.y == 1){mu_move = 2;}
+      else if(nextMoveResult.x == 0 && nextMoveResult.y == -1){mu_move = 3;}
+      else{mu_move = 0;}
+    }
+    else{mu_move = 0;}
+    
+    // Serial.println("First aimed Position x: " + String(currentPosition.x+nextMoveResult.x) + " y: " + String(currentPosition.y+nextMoveResult.y));
+
+  }
+
+  // Keeping the previous position
+  previousPosition = currentPosition;
+
+  return mu_move;
+
+}
+
+
+// Setup function
+void setup() {
+
+  //RFID pin setups
+  pinMode(TALK_PIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(TALK_PIN, LOW);
+  Serial.begin(9600);
+  SPI.begin();
+  rfid.PCD_Init();
+
+  // COMM pin setups
+  initialize_IR_module();
+  pinMode(SHIFT_REG_INPUT, OUTPUT);
+  pinMode(SHIFT_REG_CLK_PIN, OUTPUT);
+  digitalWrite(SHIFT_REG_CLK_PIN, HIGH);
+  initialize_IR_module();
+  set_active_s(2);
+
+  // Setting the obstacle positions
+  obstacles[0].x = 2;
+  obstacles[0].y = 1;
+  obstacles[1].x = 2;
+  obstacles[1].y = 5;
+  // obstacles[2].x = 3;
+  // obstacles[2].y = 4;
+    
+}
+
+void loop() {
+
+obstacleCount = initial_obstacleCount;
+ALERT_BU = 0;
+
+uint8_t mu_command = 0;
+
+MU1_state = state;
+// Communication code 
+final_communication(MU1_NextPosition, MU2_NextPosition, MU3_NextPosition, targetPosition, ID, MU1_state, MU2_state, MU3_state, buLink, active_s);
+bool movement_allowed = collision_avoidance_MU1(targetPosition, obstacles, obstacleCount, MU1_state, MU2_state, MU3_state, MU1_NextPosition, MU2_NextPosition, MU3_NextPosition);
+
+
+if(state == TARGET_REACHED){
+  mu_command = 5; // search is over
+}
+
+else{
+
+  // Checking if we are reading a new card  
+  if(rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial()) {
+
+
+    // Assign the coordinates
+    currentPosition = RFID_position_result();
+
+    // Serial.println("LED should turn on");
+
+    digitalWrite(LED_PIN, HIGH);
+    digitalWrite(TALK_PIN, HIGH);
+      
+      
+    // If current position is greater than 9, it means that that position is 
+    // the target. By subtracting 9 we get the currentPosition info as well.
+    if(currentPosition.x>9){
+      if(state < 4){
+        state = TARGET_FOUND;
+      }
+      currentPosition.x = currentPosition.x - 9;
+      currentPosition.y = currentPosition.y - 9;
+      targetPosition.x = currentPosition.x;
+      targetPosition.y = currentPosition.y;
+    }
+
+
+    if(currentPosition.x == -1){
+      // Serial.println("Failed to read the RFID card");
+      mu_command = 0;
+    }
+    else{
+      
+      if(movement_allowed){
+        mu_command = move_decider_wo_comm();
+
+        if(ALERT_BU){
+        mu_command = mu_command + 5;
+        }
+
+        uint8_t coded_coordinates = mu_command*16 + mu_command;
+
+        // Serial.println("LED PIN and TALK PIN high");
+        Serial.write(coded_coordinates);
+        delay(500);
+        digitalWrite(TALK_PIN, LOW);
+        digitalWrite(LED_PIN, LOW);
+      }
+      else{
+        // we pause the decision unit for 3000ms
+        mu_command = 0;
+        uint8_t coded_coordinates = mu_command*16 + mu_command;
+        // Serial.println("LED PIN and TALK PIN high");
+        Serial.write(coded_coordinates);
+        delay(500);
+        digitalWrite(TALK_PIN, LOW);
+        digitalWrite(LED_PIN, LOW);
+        delay(3000);
+      }
+      
+      if(MU2_state != TARGET_REACHED){
+        Position MU2_NextPosition = {20, 20};
+      }
+      if(MU3_state != TARGET_REACHED){
+        Position MU3_NextPosition = {20, 20};
+      }     
+
+    }
+  }
+
+
+  //Serial.println("Place the card to the reader...");
+  rfid.PICC_HaltA(); // Halt PICC
+  rfid.PCD_StopCrypto1(); // Stop encryption on PCD
+
+}
+
+// If target is found and BU connection is established
+if(ALERT_BU){
+  unsigned long bu_data_send_start = millis();
+  while(millis() - bu_data_send_start < 3000){
+  for(int i = 0; i<8; i++){
+    set_active_s(i);
+    send_ping_message(MU1_NextPosition, targetPosition, ID, MU1_state);
+  }
+ }
+}
+
+}
---
+++ DecisionUnArd_MU1/IR_module.ino
@@ -0,0 +1,468 @@
+//Refer to: https://www.ersinelektronik.com/class/INNOVAEditor/assets/Datasheets/TSOP1136.pdf
+
+uint8_t NUMBER_OF_PACKAGE_BYTES = 8;  //cannot be smaller than 3.
+unsigned long TRIGGER_DURATION_US = (BURST_HALF_PERIOD_US * 2) * K_NUMBER_OF_BURSTS;
+
+uint8_t IR_module_buffer[32];
+
+void initialize_IR_module() {
+  pinMode(IR_RECEIVE_PIN, INPUT);
+  pinMode(IR_LED, OUTPUT);
+}
+
+uint8_t get_buffer(uint16_t byte_index) {
+  return IR_module_buffer[byte_index];
+}
+
+// =============================================================
+uint16_t get_number_of_package_bytes() {
+  return NUMBER_OF_PACKAGE_BYTES;
+}
+void set_number_of_package_bytes(uint16_t new_package_size) {
+  NUMBER_OF_PACKAGE_BYTES = new_package_size;
+}
+
+void set_buffer(uint16_t byte_index, uint8_t byte_value) {
+  IR_module_buffer[byte_index] = byte_value;
+}
+
+void reverse_buffer() {
+  for (int j = 0; j < NUMBER_OF_PACKAGE_BYTES; j++) {
+    uint8_t reversed_byte = 0;
+    for (int i = 0; i < 8; i++) {
+      if (IR_module_buffer[j] & (1 << i)) {
+        reversed_byte |= (1 << (7 - i));
+      }
+    }
+    IR_module_buffer[j] = reversed_byte;
+  }
+}
+
+void transmit_buffer() {
+
+  reverse_buffer();
+  //=================
+
+  transmit_zero();  //start bit
+
+  for (uint16_t byte_index = 0; byte_index < NUMBER_OF_PACKAGE_BYTES; byte_index++) {
+    for (uint8_t package_bit = 0; package_bit < 8; package_bit++) {
+      uint8_t send_bit = (IR_module_buffer[byte_index] & 1);
+      if (send_bit == 0) {
+        transmit_zero();
+      } else {
+        transmit_one();
+      }
+      IR_module_buffer[byte_index] = IR_module_buffer[byte_index] >> 1;
+    }
+  }
+
+  //=================
+  reverse_buffer();
+}
+//TRANSITTERS ========================================================
+unsigned long TRANSMISSION_START_TIME = 0;
+
+void transmit_zero() {
+  TRANSMISSION_START_TIME = micros();
+  while (micros() - TRANSMISSION_START_TIME < (TRIGGER_DURATION_US-30)) {
+    // 32 us
+    digitalWrite(IR_LED, HIGH);
+    delayMicroseconds(BURST_HALF_PERIOD_US);
+    digitalWrite(IR_LED, LOW);
+    delayMicroseconds(BURST_HALF_PERIOD_US);
+  }
+}
+
+void transmit_one() {
+  TRANSMISSION_START_TIME = micros();
+  // 12 us
+  digitalWrite(IR_LED, LOW);
+  delayMicroseconds(TRIGGER_DURATION_US-10);
+}
+
+void shift_reg_insert_zero() {
+  PORTB = PORTB & B11111110; //define SHIFT_REG_INPUT 8 (portb-0)->LOW
+  //digitalWrite(SHIFT_REG_INPUT, LOW);
+  delayMicroseconds(1);
+
+  PORTB = PORTB & B11111101; //#define SHIFT_REG_CLK_PIN 9 (portb-1)->LOW
+  //digitalWrite(SHIFT_REG_CLK_PIN, LOW);
+  delayMicroseconds(1);
+
+  PORTB = PORTB | B00000010; //#define SHIFT_REG_CLK_PIN 9 (portb-1)->HIGH
+  //digitalWrite(SHIFT_REG_CLK_PIN, HIGH);
+  delayMicroseconds(1);
+}
+
+void shift_reg_insert_one() {
+  PORTB = PORTB | B00000001; //define SHIFT_REG_INPUT 8 (portb-0)->HIGH
+  //digitalWrite(SHIFT_REG_INPUT, LOW);
+  delayMicroseconds(1);
+
+  PORTB = PORTB & B11111101; //#define SHIFT_REG_CLK_PIN 9 (portb-1)->LOW
+  //digitalWrite(SHIFT_REG_CLK_PIN, LOW);
+  delayMicroseconds(1);
+
+  PORTB = PORTB | B00000010; //#define SHIFT_REG_CLK_PIN 9 (portb-1)->HIGH
+  //digitalWrite(SHIFT_REG_CLK_PIN, HIGH);
+  delayMicroseconds(1);
+}
+
+void zero_shift_register() {
+  for (uint8_t i = 0; i < 8; i++) {
+    shift_reg_insert_zero();
+  }
+}
+
+uint8_t listen_IR() {
+  digitalWrite(IR_LED, LOW);
+
+  unsigned long listen_start_time = millis();
+  uint8_t is_received = 0;
+
+  uint8_t shift_index = 0;
+  zero_shift_register();
+  while (millis() - listen_start_time < LISTEN_DURATION_MS) {
+    shift_reg_insert_one();
+    for (shift_index = 0; shift_index < 8; shift_index++) {
+      if (digitalRead(IR_RECEIVE_PIN) == 1) {
+        is_received = 1;
+        break;
+      }
+      shift_reg_insert_zero();
+    }
+    if (is_received)break;
+  }
+
+  //check if transmission is detected
+  if (is_received == 1) {
+    delayMicroseconds(TRIGGER_DURATION_US * 1.5);
+    unsigned long listen_starts = micros();
+    for (uint8_t i = 0; i < (NUMBER_OF_PACKAGE_BYTES * 8); i++) {
+      uint8_t byte_no = i / 8;
+      uint8_t read_bit = digitalRead(IR_RECEIVE_PIN);
+      if (read_bit == 1) {
+        read_bit = 0;
+      } else {
+        read_bit = 1;
+      }
+      IR_module_buffer[byte_no] = (IR_module_buffer[byte_no] << 1) + read_bit;
+      while (micros() < listen_starts + (i + 1) * TRIGGER_DURATION_US) {
+        continue;
+      }
+    }
+
+    uint16_t CRC_16 = generate_CRC_16_bit();
+    uint8_t CRC_SIG = CRC_16 >> 8;
+    uint8_t CRC_LST = CRC_16 % 256;
+    if (IR_module_buffer[NUMBER_OF_PACKAGE_BYTES - 1] == CRC_LST && IR_module_buffer[NUMBER_OF_PACKAGE_BYTES - 2] == CRC_SIG) {
+      // Serial.print(String(shift_index)+" -> ");
+      for (uint8_t i = 0; i < NUMBER_OF_PACKAGE_BYTES; i++) {
+        // Serial.print(IR_module_buffer[i]);
+        // Serial.print(' ');
+      }
+      // Serial.println();
+      return 1;  //Package is valid. return 1
+    } else {
+      // Serial.println("Package corrupted");
+      return 2;  // CRC check is failed. return 2
+    }
+  }
+
+  return 0;  // No signal is detected. return 0
+}
+
+
+//MAGICAL CRC_16 MODBUS code.
+uint16_t generate_CRC_16_bit() {
+  uint16_t remainder = CRC_16_bit_for_1BYTE(IR_module_buffer[0], 65535);
+  for (uint8_t i = 1; i < NUMBER_OF_PACKAGE_BYTES - 2; i++) {
+    remainder = CRC_16_bit_for_1BYTE(IR_module_buffer[i], remainder);
+  }
+  return remainder;
+}
+uint16_t CRC_16_bit_for_1BYTE(uint16_t data, uint16_t last_data) {
+  //if this is first data (i.e LAST_DATA==null), LAST_DATA= 65535 = FFFF
+  uint16_t key = 40961;     //1010 0000 0000 0001
+  data = data ^ last_data;  //XOR
+  for (int i = 0; i < 8; i++) {
+    boolean should_XOR = false;
+    if (data % 2 == 1) should_XOR = true;
+    data = data >> 1;
+    if (should_XOR) data = data ^ key;
+  }
+  return data;
+}
+
+
+void send_target_info_old(int currentPosition_x, int currentPosition_y, int targetPosition_x, int targetPosition_y) {
+  
+  uint8_t first_byte = currentPosition_x; 
+  uint8_t second_byte = currentPosition_y; 
+  uint8_t third_byte = targetPosition_x;
+  uint8_t fourth_byte = targetPosition_y;
+
+  set_buffer(0, first_byte);
+  set_buffer(1, second_byte);
+  set_buffer(2, third_byte);
+  set_buffer(3, fourth_byte);
+  // The 5th and 6th bits are just set to 0
+  set_buffer(4, 0);
+  set_buffer(5, 0);
+
+  uint16_t CRC_16 = generate_CRC_16_bit();
+  uint8_t CRC_SIG = CRC_16 >> 8;
+  uint8_t CRC_LST = CRC_16 % 256;
+  // Serial.println(String(CRC_SIG) + " " + String(CRC_LST));
+  set_buffer(6, CRC_SIG);
+  set_buffer(7, CRC_LST);
+
+  transmit_buffer();
+  delay(20); 
+  
+}
+
+
+//ping_test() yerine
+void send_ping_message(Position MU1_NextPosition, Position targetPosition, int ID, int MU1_state) {
+  
+  uint8_t nextPosition_x = MU1_NextPosition.x;
+  uint8_t nextPosition_y = MU1_NextPosition.y;
+  uint8_t targetPosition_x = targetPosition.x;
+  uint8_t targetPosition_y = targetPosition.y;
+
+
+  uint8_t first_byte = nextPosition_x*16 + nextPosition_y; 
+  uint8_t second_byte = MU1_state*16 + ID; 
+  uint8_t third_byte = targetPosition_x*16 + targetPosition_y;
+
+  set_buffer(0, first_byte);
+  set_buffer(1, second_byte);
+  set_buffer(2, third_byte);
+
+  uint16_t CRC_16 = generate_CRC_16_bit();
+  uint8_t CRC_SIG = CRC_16 >> 8;
+  uint8_t CRC_LST = CRC_16 % 256;
+  set_buffer(3, CRC_SIG);
+  // set_buffer(4, CRC_LST);
+    
+  transmit_buffer(); 
+  
+}
+
+// send_ping_ack yerine
+void send_ack_message(Position MU1_NextPosition, Position targetPosition, int ID, int MU1_state) {
+  
+  uint8_t nextPosition_x = MU1_NextPosition.x;
+  uint8_t nextPosition_y = MU1_NextPosition.y;
+  uint8_t targetPosition_x = targetPosition.x;
+  uint8_t targetPosition_y = targetPosition.y;
+
+  uint8_t first_byte = nextPosition_x*16 + nextPosition_y; 
+  uint8_t second_byte = (MU1_state+9)*16 + ID; 
+  uint8_t third_byte = targetPosition_x*16 + targetPosition_y;
+
+  set_buffer(0, first_byte);
+  set_buffer(1, second_byte);
+  set_buffer(2, third_byte);
+
+  uint16_t CRC_16 = generate_CRC_16_bit();
+  uint8_t CRC_SIG = CRC_16 >> 8;
+  uint8_t CRC_LST = CRC_16 % 256;
+  set_buffer(3, CRC_SIG);  
+  transmit_buffer();
+  
+}
+
+void final_communication(Position MU1_NextPosition, Position MU2_NextPosition, Position MU3_NextPosition, Position targetPosition, int ID, int MU1_state,int MU2_state,int MU3_state, int buLink, uint8_t active_s){
+
+  // We do NOT send a ping if we haven't listened to a ping for 3 loops
+  if(loop_iteration_wo_ping_message<3 || ping_time_out){
+
+    active_s = active_s + 1;
+    if(active_s == 8){active_s =0;}
+    set_active_s(active_s);
+    send_ping_message(MU1_NextPosition, targetPosition, ID, MU1_state);
+
+    // We record the last time we sent a ping
+    ping_timer = millis();
+
+  }
+
+  if(millis()-ping_timer < 100){
+    ping_time_out = 0;
+  }
+  // If we haven't been sending pings for 500 ms
+  else{ping_time_out = 1;}
+
+
+  if(active_s == 4){
+    // Serial.print("LOOP ITERATION COUNT WO PING: ");
+    // Serial.println(loop_iteration_wo_ping_message);
+    loop_iteration_wo_ping_message++;
+  }
+
+  listener(MU1_NextPosition, MU2_NextPosition, MU3_NextPosition, targetPosition,MU1_state,MU2_state, MU3_state, buLink, loop_iteration_wo_ping_message);
+
+}
+
+void listener(Position MU1_NextPosition, Position MU2_NextPosition, Position MU3_NextPosition, Position targetPosition,int MU1_state,int MU2_state, int MU3_state,int buLink, uint8_t loop_iteration_wo_ping_message){
+      
+  // We listen for 20 ms at a time
+  uint8_t listening_result = listen_IR_Erdem(); // 0:no package, 1:successful package, 2:corrupted package
+  if(listening_result == 2){listening_result = listen_IR_Erdem();}
+  
+  if (listening_result == 1){ // i.e., the message received is succesful
+    
+    uint8_t  received_state = 0;
+
+    uint8_t first_byte = get_buffer(0);
+    uint8_t second_byte = get_buffer(1);
+    uint8_t third_byte = get_buffer(2);
+
+    uint8_t  received_x = first_byte >> 4;
+    uint8_t  received_y = first_byte % 16;
+
+    uint8_t  intention = second_byte >> 4;
+    uint8_t  received_ID = second_byte % 16;
+    uint8_t  target_x = third_byte >> 4;
+    uint8_t  target_y = third_byte % 16;
+    
+    // Determining the message intention
+    if(intention>10){
+      received_state = intention-9;
+      intention = 2;
+    }
+    else{
+      received_state = intention;
+      intention = 1;
+    }
+
+
+    // We update the info we have on the units depending on the info we received.
+    if(received_ID == 2){
+      MU2_NextPosition.x = received_x;
+      MU2_NextPosition.y = received_y;
+      MU2_state = received_state;
+      if(target_x < 10 && targetPosition.x > 10){
+        targetPosition.x = target_x;
+        targetPosition.y = target_y;
+      }
+    }  
+    else if(received_ID == 3){
+      MU3_NextPosition.x = received_x;
+      MU3_NextPosition.y = received_y;
+      MU3_state = received_state;
+      if(target_x < 10 && targetPosition.x > 10){
+        targetPosition.x = target_x;
+        targetPosition.y = target_y;
+      }
+    }
+    else if(received_ID == 4){
+      
+      buLink = 1;
+      if(target_x < 10 && targetPosition.x > 10){
+        targetPosition.x = target_x;
+        targetPosition.y = target_y;
+      }
+
+    }   
+
+    if(intention == 1){
+
+      loop_iteration_wo_ping_message = 0;
+      // Serial.println("Ping PACKAGE is received, sending ping ack");
+      send_ack_message(MU1_NextPosition, targetPosition, ID, MU1_state);  
+      send_ack_message(MU1_NextPosition, targetPosition, ID, MU1_state); 
+                       
+      return;
+    }
+
+    else if(intention == 2){ // Acknowledgement is received
+      // Serial.println("Ping ACKNOWLEDGEMENT is received.");
+      return;
+    }
+  }
+  
+}
+
+void set_active_s(uint8_t pick_this_s) {
+  
+  int led_to_i_mapping[9] = { 4, 5, 6, 7, 0, 1, 2, 3, 999 };  // do not alter, physically linked
+  digitalWrite(SHIFT_REG_INPUT, LOW);
+  for (uint8_t i = 0; i < 8; i++) {
+    if (i == led_to_i_mapping[pick_this_s]) {
+      digitalWrite(SHIFT_REG_INPUT, HIGH);
+    } else {
+      digitalWrite(SHIFT_REG_INPUT, LOW);
+    }
+    delayMicroseconds(5);
+
+    digitalWrite(SHIFT_REG_CLK_PIN, LOW);
+    delayMicroseconds(25);
+    digitalWrite(SHIFT_REG_CLK_PIN, HIGH);
+    delayMicroseconds(25);
+  }
+}
+
+uint8_t listen_IR_Erdem() {
+  digitalWrite(IR_LED, LOW);
+
+  unsigned long listen_start_time = millis();
+  uint8_t is_received = 0;
+
+  uint8_t shift_index = 0;
+  zero_shift_register();
+  while (millis() - listen_start_time < LISTEN_DURATION_MS) {
+    shift_reg_insert_one();
+    for (shift_index = 0; shift_index < 8; shift_index++) {
+      
+      if (digitalRead(IR_RECEIVE_PIN) == 1) {
+        is_received = 1;
+        break;
+      }
+      shift_reg_insert_zero();
+    }
+    if (is_received)break;
+  }
+
+  //check if transmission is detected
+  if (is_received == 1) {
+    delayMicroseconds(TRIGGER_DURATION_US * 1.5);
+    unsigned long listen_starts = micros();
+    for (uint8_t i = 0; i < (NUMBER_OF_PACKAGE_BYTES * 8); i++) {
+      uint8_t byte_no = i / 8;
+      uint8_t read_bit = digitalRead(IR_RECEIVE_PIN);
+      if (read_bit == 1) {
+        read_bit = 0;
+      } else {
+        read_bit = 1;
+      }
+      IR_module_buffer[byte_no] = (IR_module_buffer[byte_no] << 1) + read_bit;
+      while (micros() < listen_starts + (i + 1) * TRIGGER_DURATION_US) {
+        continue;
+      }
+    }
+
+    uint16_t CRC_16 = generate_CRC_16_bit();
+    uint8_t CRC_SIG = CRC_16 >> 8;
+    uint8_t CRC_LST = CRC_16 % 256;
+    // if (IR_module_buffer[NUMBER_OF_PACKAGE_BYTES - 1] == CRC_LST && IR_module_buffer[NUMBER_OF_PACKAGE_BYTES - 2] == CRC_SIG) {
+    if (IR_module_buffer[NUMBER_OF_PACKAGE_BYTES - 1] == CRC_SIG) {      
+      // Serial.print(String(shift_index)+" -> ");
+      for (uint8_t i = 0; i < NUMBER_OF_PACKAGE_BYTES; i++) {
+        Serial.print(IR_module_buffer[i]);
+        Serial.print(' ');
+      }
+      // Serial.println();
+      return 1;  //Package is valid. return 1
+    } else {
+      Serial.println("Package corrupted");
+      return 2;  // CRC check is failed. return 2
+    }
+  }
+
+  return 0;  // No signal is detected. return 0
+}
+
