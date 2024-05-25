bfd317986a387b1e29550a7111d8c121e076f123
Erdem <erdemcanaz@gmail.com>
2024-05-25 21:27:50 +0300

    UI

---
+++ observer_UI/objects.py
@@ -0,0 +1,113 @@
+import numpy as np
+import time
+
+class Unit():
+
+    def __init__(self, id:int=0, icon_image:np.ndarray=None, x:int=1, y:int=1):
+
+        self.ICON_IMAGE = icon_image
+        self.ID = id # ID of the mobile unit
+
+        self.x = x # x coordinate of the mobile unit
+        self.y = y # y coordinate of the mobile unit
+        self.x_target = None # x coordinate of the target of the mobile unit
+        self.y_target = None # y coordinate of the target of the mobile unit
+        self.state = "NA" # state of the mobile unit
+
+        self.number_of_packages = 0 # number of packages that the mobile unit has sent and received
+        self.last_time_package_received = None # time when the last package wasa sent by the mobile unit and received
+    
+    def should_draw(self):
+        if self.x is None or self.y is None:
+            return False
+        if self.x == 15 and self.y == 15:
+            return False
+        return True
+    
+    def get_icon(self):
+        return self.ICON_IMAGE
+    
+    def get_coordinates(self):
+        return (self.x, self.y)
+    
+    def update_with_info(self, info_dict:dict):
+        id = info_dict['received_ID']
+        if id != self.ID:
+            return
+        
+        self.number_of_packages += 1
+        
+        self.x = info_dict['x']
+        self.y = info_dict['y']
+        
+        self.x_target = info_dict['target_x']
+        self.y_target = info_dict['target_y']
+        self.state = info_dict['state']
+
+        self.last_time_package_received = time.time()
+        pass
+
+    def get_id(self):
+        return self.ID
+    
+    def return_elapsed_time_since_last_package(self):
+        if self.last_time_package_received is None:
+            return "NA"
+        
+        elapsed_time_s = time.time() - self.last_time_package_received
+
+        if elapsed_time_s < 60:
+            return f"{(elapsed_time_s):.2f} s"
+        
+        elapsed_time_m = elapsed_time_s / 60
+        if elapsed_time_m < 60:
+            return f"{int(elapsed_time_m)} m"
+        
+        elapsed_time_h = elapsed_time_m / 60
+        return f"{int(elapsed_time_h)} h"
+        
+    def get_state(self):
+        return self.state
+    
+        
+class Obstacle():
+
+    def __init__(self, x:int, y:int, icon_image:np.ndarray):
+        self.ICON_IMAGE = icon_image
+        self.x = x # x coordinate of the obstacle
+        self.y = y # y coordinate of the obstacle
+
+    def get_icon(self):
+        return self.ICON_IMAGE
+    
+    def get_coordinates(self):
+        return (self.x, self.y)
+    
+
+class Target():
+
+    def __init__(self, x:int, y:int, found_target_icon:np.ndarray, not_found_target_icon:np.ndarray):
+        self.x = x # x coordinate of the target
+        self.y = y
+        self.found = False
+        self.found_target_icon = found_target_icon
+        self.not_found_target_icon = not_found_target_icon
+            
+    def set_found_status(self, found:bool):
+        self.found = found
+
+    def get_coordinates(self):
+        return (self.x, self.y)
+    
+    def get_icon(self):
+        if self.found:
+            return self.found_target_icon
+        else:
+            return self.not_found_target_icon
+
+
+
+
+
+
+
